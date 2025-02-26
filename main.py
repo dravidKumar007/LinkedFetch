804d8483476ed9eb38dbd8c23675910797ea1a02
Dravid Kumar <132421260+dravidKumar007@users.noreply.github.com>
2025-02-26 20:23:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from pymongo import MongoClient
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
 from decouple import config
 from starlette.responses import RedirectResponse
-from questions import  router as questions_router
+from questions import router as questions_router
 from auth import router as auth_router
+
 app = FastAPI()
+
+# Enable CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all HTTP methods
+    allow_headers=["*"],  # Allows all headers
+)
+
 app.include_router(questions_router)
 app.include_router(auth_router)
+
 # Retrieve MongoDB URI from environment variable
 mongo_uri = "mongodb+srv://xohack:w4BRF5QXTHUcbCPE@xohack-cluster.0asnq.mongodb.net/?retryWrites=true&w=majority&appName=xohack-cluster"
 
@@ -17,14 +30,11 @@ client = MongoClient(mongo_uri)
 db = client["auth_db"]
 users_collection = db["users"]
 
-
 # Define response model for LinkedIn data
 class LinkedInData(BaseModel):
-    # Define the expected structure for LinkedIn data
     profile_url: str
     experience: str
     skills: list[str]
-    # You can extend this as needed
 
 @app.get("/")
 async def root():
